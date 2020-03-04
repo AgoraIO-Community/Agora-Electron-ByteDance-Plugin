656b01b78395fa4cceeea48463a7cec63bd3f995
your name <your email>
2020-03-04 14:21:54 +0800

    add artifacts

--- ByteDance290101/win/build.sh
+++ ByteDance290101/win/build.sh
@@ -2,9 +2,10 @@
 rm -rf lab_tob*
 rm -rf glew
 rm -rf bytedance
+rm -rf Release
 
 unzip -u bytedance.zip
 unzip -u glew.zip
 mkdir bytedance
-mv lab_tob*/* bytedance
-mv glew bytedance
+mv lab_tob*/* bytedance/.
+mv glew bytedance/.
--- ByteDance290101/win/package.sh
+++ ByteDance290101/win/package.sh
@@ -1,1 +1,3 @@
+cp ./bytedance/glew/**/*.dll ./Release/.
+cp ./bytedance/effect/**/*.dll ./Release/.
 7z a -r -tzip bytedance-win.zip ./Release
