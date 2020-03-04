94f3965db49ec06da990a7c277dd112be19141d9
your name <your email>
2020-03-04 15:29:41 +0800

    fix

--- ByteDance290101/win/package.sh
+++ ByteDance290101/win/package.sh
@@ -1,5 +1,5 @@
 ls -alt
 ls -alt ./bytedance
-cp ./bytedance/glew/**/*.dll ./Release/.
-cp ./bytedance/effect/**/*.dll ./Release/.
-7z a -r -tzip bytedance-win.zip ./Release
+cp ./bytedance/glew/bin/Release/Win32/*.dll ./Release/.
+cp ./bytedance/effect/libs/msvc14/Win32/Release/*.dll ./Release/.
+7z a -r -tzip bytedance-win.zip ./Release/.
