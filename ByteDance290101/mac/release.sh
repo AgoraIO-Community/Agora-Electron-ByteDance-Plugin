775badfa05415b63a8e04d26056f1a99a61b39cf
QianzeZhang <zhangqianze@agora.io>
2020-03-04 16:59:32 +0800

    fix catalina issue

--- ByteDance290101/mac/release.sh
+++ ByteDance290101/mac/release.sh
@@ -6,6 +6,8 @@ unzip -u bytedance.zip
 mkdir -p bytedance/byted_effect_mac_headers
 unzip -u byted_effect*/byted_effect_mac_headers.zip -d bytedance/byted_effect_mac_headers
 
+xattr -r -d com.apple.quarantine byted_effect*/libeffect.dylib
+
 cp byted_effect*/libeffect.dylib bytedance/
 
 rm -rf build
