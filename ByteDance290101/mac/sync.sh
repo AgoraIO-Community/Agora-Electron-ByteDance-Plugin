02a42a4530f456fdc206e309a0469dc96cbc8f84
QianzeZhang <zhangqianze@agora.io>
2020-04-23 12:21:13 +0800

    fix openssl

--- ByteDance290101/mac/sync.sh
+++ ByteDance290101/mac/sync.sh
@@ -1,2 +1,3 @@
 rm -rf bytedance.zip
 wget https://github.com/AgoraIO-Community/Agora-Electron-ByteDance-Plugin/releases/download/3.0.1/byted_effect_1449_mac.zip -O bytedance.zip
+wget https://github.com/AgoraIO-Community/Agora-Electron-ByteDance-Plugin/releases/download/3.0.1/openssl-mac.zip -O openssl.zip
